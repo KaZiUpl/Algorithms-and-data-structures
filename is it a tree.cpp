4f2e612d3e1ecd087845235e65a8cf33ad0c1c35
KaZiUpl <38405516+KaZiUpl@users.noreply.github.com>
2018-04-22 12:43:34 +0200

    Finally

--- is it a tree.cpp
+++ is it a tree.cpp
@@ -2,59 +2,138 @@
 #include<vector>
 #include<queue>
 #include<set>
-
 using namespace std;
 
-bool czy_jest(vector<int>kierunki,int wartosc)
+class Node
 {
-    for(int i=0;i<kierunki.size();++i)
+public:
+    int start;
+    vector<int> ends;
+};
+int wyszukaj_poczatek(vector<Node>graf,int start) /// zwraca indeks wierzcholka w grafie lub -1
+{
+    int i;
+    for(i=0;i<graf.size();++i)
     {
-        if(kierunki[i] == wartosc) return true;
+        if(start == graf[i].start) return i;
     }
-    return false;
+    return -1;
+}
+int znajdz_korzen(vector<Node>graf,set<int>wierzcholki)
+{
+    int korzen=0;
+    set <int> konce;
+    std::set<int>::iterator it;
+    int i,j;
+    for(i=0;i<graf.size();++i)
+    {
+        for(j=0;j<graf[i].ends.size();++j)
+        {
+            konce.insert(graf[i].ends[j]);
+        }
+    }
+    for (it=wierzcholki.begin(); it!=wierzcholki.end(); ++it)
+    {
+        if(konce.find(*it) == konce.end()) korzen = *it;
+    }
+
+    return korzen;
+}
+void DFS_odwiedz(int wierzcholek, queue <int> &kolejka, vector<bool> &sprawdzone,vector<Node> graf)
+{
+    int i;
+    for(i = 0;i<graf[wierzcholek].ends.size();++i) // dla kazdego dziecka danego wierzcholka
+    {
+        if(wyszukaj_poczatek(graf,graf[wierzcholek].ends[i]) != -1)
+        {
+            //cout<<"jestem przed ifem, szukam "<<graf[wierzcholek].ends[i]<<endl;
+            if(!sprawdzone[ wyszukaj_poczatek(graf,graf[wierzcholek].ends[i]) ])
+            {
+                //cout<<"jestem w ifie"<<endl;
+                kolejka.push( wyszukaj_poczatek(graf,graf[wierzcholek].ends[i]) );
+            }
+        }
+
+    }
+    sprawdzone[wierzcholek] = true;
+    kolejka.pop();
+}
+void DFS(vector<bool> &sprawdzone,vector<Node> graf,int korzen)
+{
+    queue <int> kolejka;
+    int i;
+    kolejka.push(korzen);
+    while(!kolejka.empty())
+    {
+        DFS_odwiedz(kolejka.front(),kolejka,sprawdzone,graf);
+    }
 }
 
 int main()
 {
     int case_number = 1,
-        first_point, second_point;
-    vector<vector <int> > vertex_vec;
+        first_point, second_point,
+        arc_count;
 
-    while(cin>>first_point>>second_point && (first_point>0 && second_point>0))
+
+    while(cin>>first_point>>second_point && (first_point>=0 && second_point>=0))
     {
-        bool jest = false;
-        vector<int> kierunki;
+        bool jest = true;
+        vector< Node > graf;
+        vector<bool> sprawdzone;
+        set<int>wierzcholki;
+        arc_count = 0;
 
         while(first_point != 0 || second_point != 0)
         {
-            cout<<first_point<<" "<<second_point<<endl;
-            int i;
-            for(int i=0; i<vertex_vec.size();++i)
+            if(first_point == second_point) jest = false;
+            //cout<<first_point<<" "<<second_point<<endl;
+            if(wyszukaj_poczatek(graf,first_point) == -1)
             {
-                if(vertex_vec[i][0] == first_point) vertex_vec[i].push_back(second_point);
-                else
-                {
-                    vector<int> new_vec;
-                    new_vec.push_back(first_point);
-                    new_vec.push_back(second_point);
-                    vertex_vec.push_back(new_vec);
-                }
+                Node temp;
+                temp.start = first_point;
+                temp.ends.push_back(second_point);
+                graf.push_back(temp);
+                sprawdzone.push_back(false);
+                arc_count++;
+                wierzcholki.insert(first_point);
+                wierzcholki.insert(second_point);
             }
-            for(int g=0;g<vertex_vec.size();++g)
+            else
             {
-                cout<<"Start: "<<vertex_vec[g][0]<<" ends: ";
-                for(int h=1;h<vertex_vec[g].size();++h)
+                graf[wyszukaj_poczatek(graf,first_point)].ends.push_back(second_point);
+                wierzcholki.insert(second_point);
+                arc_count++;
+            }
+            //cout<<"graf:"<<endl;
+            for(int g=0;g<graf.size();g++)
+            {
+                /*
+                cout<<"Start: "<<graf[g].start<<endl;
+                for(int h=0;h<graf[g].ends.size();++h)
                 {
-                    cout<<vertex_vec[g][h]<<" ";
+                    cout<<graf[g].ends[h]<<" ";
                 }
                 cout<<endl;
+                */
             }
+            //cout<<"vertex,arc: "<<wierzcholki.size()<<" "<<arc_count<<endl;
             cin>>first_point>>second_point;
+        }
+        if(wierzcholki.size()-1 != arc_count) jest = false;
+        else
+        {
+            DFS(sprawdzone,graf,wyszukaj_poczatek(graf,znajdz_korzen(graf,wierzcholki)));
+            int j;
+            for(j=0;j<sprawdzone.size();++j)
+            {
+                jest = jest && sprawdzone[j];
+            }
         }
 
         cout<<"Case "<<case_number++;
-        if(jest) cout<<" is not a tree"<<endl;
-        else cout<<" is  a tree"<<endl;
+        if(jest || wierzcholki.empty()) cout<<" is a tree."<<endl;
+        else cout<<" is not a tree."<<endl;
     }
 
 
